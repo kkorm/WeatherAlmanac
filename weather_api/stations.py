b761277662ccbe69bdc87779b59af16ee4d7df02
kkorm <112270089+kkorm@users.noreply.github.com>
2023-01-02 11:16:06 -0600

    Cleanup stations.py code

--- weather_api/stations.py
+++ weather_api/stations.py
@@ -14,6 +14,5 @@ class stations:
         
         request_path = "https://api.weather.gov/offices/" + office_id
         json_request = json.loads(requests.get(request_path).text)
-        station_list = json_request["approvedObservationStations"]
         station_list = [x.split("/K")[1] for x in json_request["approvedObservationStations"]]
         return station_list
